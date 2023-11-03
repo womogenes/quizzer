82264def7e5c8e3376db2fb5d8cf95302f5c27a1
William Y. Feng <womogenes2@gmail.com>
2023-11-02 22:29:44 -0700

    Add mobile responsiveness

--- js/index.js
+++ js/index.js
@@ -1,10 +1,32 @@
 const $ = document.querySelector.bind(document);
 
+const typograph = (str) => {
+  const runReplacements = (content, replacements) => {
+    for (let [og, repl] of replacements) {
+      content = content.replace(og, repl);
+    }
+    return content;
+  };
+  const replacements = [[/"([^"]+)?"/g, (_, p1) => `“${p1}”`]];
+  return runReplacements(str, replacements);
+};
+
 document.addEventListener('alpine:init', () => {
+  Alpine.store('curQues', {});
+  Alpine.store('quizLoaded', false);
+  console.log('woah!');
+});
+
+document.addEventListener('alpine:init', async () => {
   console.log('Alpine initialized.');
+
+  console.log('Loading quiz questions...');
+  const questions = await fetchSpreadsheet('Questions');
+  console.log(questions);
 
   Alpine.store('curIdx', 0);
   Alpine.store('questions', questions);
+  Alpine.store('quizLoaded', true);
 
   const loadQuestion = (questionArr) => {
     const [imageURL, question, options, answer] = questionArr;
@@ -12,11 +34,10 @@ document.addEventListener('alpine:init', () => {
     // Load the ith question
     Alpine.store('curQues', {
       imageURL,
-      question,
+      question: typograph(question),
       options,
-      answer,
+      answer: parseInt(answer),
     });
-    console.log(Alpine.store('curQues'));
   };
 
   window.answer = (index) => {
@@ -24,16 +45,18 @@ document.addEventListener('alpine:init', () => {
       'answerStatus',
       index === Alpine.store('curQues').answer ? 'correct' : 'incorrect',
     );
+    Alpine.store('chosenOption', index);
     $('#options-grid').style.pointerEvents = 'none';
-    $('#options-grid').style.opacity = 0.8;
+    $('#options-grid').style.opacity = 0.9;
 
     window.setTimeout(() => {
       $('#options-grid').style.pointerEvents = 'auto';
-      $('#options-grid').style.opacity = 1;
+      $('#options-grid').style.opacity = 1.0;
+      $('#image').src = '';
       Alpine.store('answerStatus', 'answering');
-      Alpine.store('curIdx', Alpine.store('curIdx') + 1);
+      Alpine.store('curIdx', (Alpine.store('curIdx') + 1) % questions.length);
       loadQuestion(questions[Alpine.store('curIdx')]);
-    }, 1000);
+    }, 2000);
   };
 
   loadQuestion(questions[Alpine.store('curIdx')]);

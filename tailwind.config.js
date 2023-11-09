d3e20bc3281d6d03567df9c109f8450fda128765
William Y. Feng <womogenes2@gmail.com>
2023-11-08 17:30:04 -0800

    Add theme color, other visuals

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
       sans: ['Inter', 'ui-sans-serif', 'sans-serif'],
       serif: ['Georgia', 'Times New Roman'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        roman: {
+          50: '#fef2f2',
+          100: '#fde3e3',
+          200: '#fccccc',
+          300: '#f9a8a8',
+          400: '#f47575',
+          500: '#eb5555',
+          600: '#d62c2c',
+          700: '#b42121',
+          800: '#951f1f',
+          900: '#7c2020',
+          950: '#430c0c',
+        },
+      },
+    },
   },
   plugins: [],
 };
